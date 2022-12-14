9169a25b1ca3f8203882ee1d5888d037cb36fc35
Karl August Nestor <jesuscaesar@outlook.com>
2022-12-14 02:29:52 +0000

    Improved functionality

--- ru.metro.php
+++ ru.metro.php
@@ -13,7 +13,6 @@ $metro['ru'] =
         "height" => "Рост",
         "weight" => "Вес",
         "size" => "Размер",
-        "arousal" => "Красота",
     ],
     "unit" =>
     [
@@ -21,6 +20,20 @@ $metro['ru'] =
         "height" => "м",
         "weight" => "кг",
         "size" => "RU",
+    ],
+    "side" =>
+    [
+        "quantity" => "right",
+        "height" => "right",
+        "weight" => "right",
+        "size" => "right",
+    ],
+    "space" =>
+    [
+        "quantity" => "",
+        "height" => " ",
+        "weight" => " ",
+        "size" => " ",
     ],
     "round" =>
     [
